a1533a66ba31f9bd452f1f72120e7e74fcb56f98
HKLCF <hklcf@users.noreply.github.com>
2018-05-06 18:51:38 +0800

    Create mediafire.php

---
+++ mediafire.php
@@ -0,0 +1,35 @@
+<?php
+/*
+    Version: 1.0
+    Author: HKLCF
+    Copyright: HKLCF
+    Last Modified: 06/05/2018
+*/
+
+$url = htmlspecialchars($_GET['url']);
+$support_domain = 'www.mediafire.com';
+
+if(empty($url)) {
+  $url = 'http://www.mediafire.com/file/8x5ol3r8wpb477a/small.mp4'; // sample link
+}
+if($url) {
+  preg_match('@^(?:http?://)?([^/]+)@i', $url, $matches);
+  $host = $matches[1];
+  if($host != $support_domain) {
+    echo 'Please input a valid google drive url.';
+    exit;
+  }
+}
+
+$result = file_get_contents($url, false, stream_context_create(['socket' => ['bindto' => '0:0']])); // force IPv4
+
+preg_match('/kNO = "(.*)"/', $result, $matches);
+$result = urldecode($matches[1]);
+
+$output = [];
+$output[] = ['label' => 'Original', 'file' => $result, 'type' => 'video/mp4'];
+
+$output = json_encode($output);
+
+echo $output;
+?>
